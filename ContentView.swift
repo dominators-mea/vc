3ec33b6f66f14a2c9269362e852aa4fcc95cd5ed
Void <jomatahunde@gmail.com>
2026-05-25 05:25:29 +0100

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello IPAGURU!")
+                .font(.largeTitle)
+                .bold()
+            Text("Your app works 🎉")
+        }
+    }
+}
---
+++ IPAGURUApp.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+@main
+struct IPAGURUApp: App {
+    var body: some Scene {
+        WindowGroup { ContentView() }
+    }
+}
