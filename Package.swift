7f9968498269ecfb3f495cb1e0a90b6a80517426
Void <jomatahunde@gmail.com>
2026-05-25 06:23:32 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,20 +3,13 @@ import PackageDescription
 
 let package = Package(
     name: "IPAGURU",
-    platforms: [.iOS(.v16)],
-    products: [
-        .iOSApplication(
-            name: "IPAGURU",
-            targets: ["App"],
-            bundleIdentifier: "com.ipaguru.app"
-        )
+    platforms: [
+        .iOS(.v15)
     ],
     targets: [
         .target(
             name: "App",
-            path: ".",
-            sources: ["IPAGURUApp.swift", "ContentView.swift"],
-            resources: [.process("Info.plist")]
+            path: "."
         )
     ]
 )
